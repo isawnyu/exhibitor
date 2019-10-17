f6e68309b612d22cca284b9ac82f005e663b0e36
Tom Elliott <tom.elliott@nyu.edu>
2019-10-17 13:50:53 -0500

    Set up default python package structure using newproj.sh.

---
+++ README.md
@@ -0,0 +1,6 @@
+# exhibitor
+
+Prepare data for ISAW website exhibitions
+
+exhibitor is the work of Tom Elliott (tom.elliott@nyu.edu). Copyright (c) 2019 New York University. See LICENSE.txt file for complete license.
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="exhibitor",
+    version="0.0.1",
+    author="Tom Elliott",
+    author_email="tom.elliott@nyu.edu",
+    description="Prepare data for ISAW website exhibitions",
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8.0",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['airtight', 'better_exceptions'],
+    python_requires='>=3.8.0'
+)
